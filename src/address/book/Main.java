24df339b642de565e46514194ca13b5998a5e42b
Pooja <pooja.14206@gmail.com>
2021-04-09 23:08:01 +0530

    [POOJA] Ability to create a Contacts in Address

--- src/address/book/Main.java
+++ src/address/book/Main.java
@@ -1,8 +1,11 @@
 package address.book;
+import address.book.Contact;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Wellcome to Address Book program.");
+		
+		Contact storeValue = new Contact("john", "kennedy", "Gauhati", "Assam", "xyz14@gmail.in", 78325, 100);
+		storeValue.Display();
 	}
-
 }
