1e5679345b6ed7ba20e999153d7e4553fc5e29d5
Pooja <pooja.14206@gmail.com>
2021-04-13 10:04:08 +0530

    [POOJA] modify the file of AddContact.

---
+++ src/address/book/AddContactNew.java
@@ -0,0 +1,13 @@
+package address.book;
+
+public class AddContactNew {
+	String firstName;
+	String lastName;
+	String city;
+	String state;
+	String email;
+	int zip;
+	int phoneNo;
+
+
+}
--- src/address/book/Contact.java
+++ src/address/book/Contact.java
@@ -1,6 +1,5 @@
 package address.book;
 
-import java.util.Scanner;
 
 public class Contact{
 		String firstName;
@@ -31,26 +30,5 @@ public class Contact{
 			System.out.println("EmailId: " + this.email);
 		}
 		
-		public void NewContact(){
-			Scanner newData = new Scanner(System.in);   
-			System.out.println("Add a new Contact to ADDRESS BOOK:------");
-			System.out.print("First Name: ");  
-			String firstName= newData.nextLine();  
-			System.out.print("Last Name: ");  
-			String lastName= newData.nextLine();  
-			System.out.print("City: ");  
-			String city= newData.nextLine();
-			System.out.print("State: ");  
-			String state= newData.nextLine();
-			System.out.print("EmailID: ");  
-			String email= newData.nextLine();
-			System.out.print("Zip: ");  
-			int zip= newData.nextInt();
-			System.out.print("PhoneNumber: ");  
-			int phoneNo= newData.nextInt();
-			System.out.println("First Name: " + firstName + "\nLast Name: " + lastName + "\nCity: " + city + "\nState: " + state + "\nZip: " + zip + "\nPhone Number: " +phoneNo + "\nEmailID: " + email);
-			
-			newData.close(); //to stop worning:resource lake newdata is never closed(not close Scanner)
-		}  
-			
-}
+		
+		}
